ec2f0ab3522c33c0f608d231daa81f4ce223aa8b
Eduardo Bergamo <ebergammo@hotmail.com>
2023-01-31 22:00:42 -0300

    finalizando o exercicio

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const form = document.getElementById('form-enviar')
 
+function validaForm (numA, numB) {
+    return parseInt(numB) > parseInt(numA) 
+}
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    const numeroA = document.getElementById('numeroA')
-    const numeroB = document.getElementById('numeroB')
-
-
-    function validaForm (numeroA, numeroB) {    
-        if (numeroA < numeroB) {
-            alert("Form valido")
-        } else {
-            alert("Form invalido")
-        }
-    }
+    const valorA = document.getElementById('numeroA')
+    const valorB = document.getElementById('numeroB')
+    const mensagemSucesso = "Formulário válido, campo B é maior que o campo A"
+    const mensagemErro = "Formulário inválido, campo A é maior que o campo B"
     
+    let formValido = false
+    formValido = validaForm(valorA.value, valorB.value)
+
+    if (formValido) {
+        alert(mensagemSucesso)
+    } else {
+        alert(mensagemErro)
+    }
+
     validaForm(numeroA, numeroB)
     
 })
